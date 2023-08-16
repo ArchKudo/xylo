a05f63e179315cd9e27a4ec90cd55e8eb58cbea3
Girish Vλas [gdv1] <gdv1@aber.ac.uk>
2023-08-16 19:26:00 +0100

    Fix linter checks

--- scripts/aln.sh
+++ scripts/aln.sh
@@ -12,7 +12,7 @@
 
 # Setup checks
 srun /bin/hostname
-srun echo $PATH
+srun echo "$PATH"
 srun pwd
 srun which fasterq-dump
 srun which bowtie2
@@ -24,15 +24,15 @@ ls -R "${setup[@]}"
 
 function align {
     echo "$0ing $1"
-    fasterq-dump ${1} --outdir pairs/ --temp tmp/ \
+    fasterq-dump "${1}" --outdir pairs/ --temp tmp/ \
     --bufsize 10MB --curcache 100MB --mem 4000MB --threads 16 \
     --progress --verbose --details --log-level debug && \
     bowtie2 --threads 16 --time -x db/xylo \
         -q --phred33 --local --very-sensitive-local --no-unal \
         -N 1 -L 12 --rfg 5,2 \
-        -1 pairs/${1}_1.fastq -2 pairs/${1}_2.fastq -S aln/${1}.sam && \
+        -1 pairs/"${1}"_1.fastq -2 pairs/"${1}"_2.fastq -S aln/"${1}".sam && \
     rm -f pairs/${1}_{1,2}.fastq && \
-    echo "Aligned $1"
+    echo "$0ed $1"
 }
 
 export -f align

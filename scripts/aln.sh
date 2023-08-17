7f0e45d10a6ccae8072ba85df9c536d06bfa0f7e
Girish Vλas [gdv1] <gdv1@aber.ac.uk>
2023-08-17 03:39:52 +0100

    Lint fix

--- scripts/aln.sh
+++ scripts/aln.sh
@@ -31,36 +31,32 @@ function align {
     echo "Starting alignment for $1"
 
     # Dump fastq files
-    fasterq-dump "${1}" --outdir pairs/ --temp tmp/ \
+    if fasterq-dump "${1}" --outdir pairs/ --temp tmp/ \
     --bufsize 10MB --curcache 100MB --mem 4000MB --threads 16 \
-    --progress --verbose --details --log-level debug
-
-    # Check if fasterq-dump command succeeded
-    if [ $? -eq 0 ]; then
-        echo "Downloaded fastq files for $1"
-    else
-        echo "Download failed for $1"
-        exit 1
+    --progress --verbose --details --log-level debug;
+        then
+            echo "Downloaded fastq files for $1"
+        else
+            echo "Download failed for $1"
+            exit 1
     fi
 
     # Run bowtie2 on downloaded files
     if [ -f "pairs/$1_1.fastq" ] && [ -f "pairs/$1_2.fastq" ]; then
-        bowtie2 --threads 16 --time -x db/xylo \
+        if bowtie2 --threads 16 --time -x db/xylo \
             -q --phred33 --local --very-sensitive-local --no-unal \
             -N 1 -L 12 --rfg 5,2 \
-            -1 pairs/"$1"_1.fastq -2 pairs/"$1"_2.fastq -S aln/"$1".sam
+            -1 "pairs/$1_1.fastq" -2 "pairs/$1_2.fastq" -S aln/"$1".sam;
+            then
+                echo "Bowtie2 align completed for $1"
+                echo "Deleting fastq files"
+                rm -f pairs/"$1"_{1,2}.fastq
+            else
+                echo "Bowtie2 align failed for $1"
+                exit 1
+            fi
     else
         echo "Incomplete pairs present for $1"
-        exit 1
-    fi
-
-    # Check if bowtie2 command succeeded
-    if [ $? -eq 0 ]; then
-        echo "Bowtie2 align completed for $1"
-        echo "Deleting fastq files"
-        rm -f pairs/$1_{1,2}.fastq
-    else
-        echo "Bowtie2 align failed for $1"
         exit 1
     fi
 
@@ -70,6 +66,6 @@ function align {
 
 export -f align
 
-srun parallel --joblog logs/parallel --tmpdir $TMPDIR \
-    --compress --keep-order --line-buffer \
+srun parallel --joblog logs/parallel --tmpdir tmp/ \
+    --compress --keep-order --group \
     --retries 3 --jobs 2 --arg-file runs align

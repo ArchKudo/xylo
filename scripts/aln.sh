64376237c5825803cd64b7cecd517e6915277a6f
Girish Vλas [gdv1] <gdv1@aber.ac.uk>
2023-08-17 16:53:23 +0100

    prefetch sra files ; format

--- scripts/aln.sh
+++ scripts/aln.sh
@@ -22,50 +22,63 @@ srun echo $TMPDIR
 srun ls $TMPDIR
 
 # Create required setup directories
-declare -a setup=(pairs tmp aln logs)
+declare -a setup=(data pairs tmp aln logs)
 mkdir -p "${setup[@]}"
 ls -R "${setup[@]}"
 
 # get fastq && align && delete fastq
 function align {
     echo "Starting alignment for $1"
-
-    # Dump fastq files
-    if fasterq-dump "${1}" --outdir pairs/ --temp tmp/ \
-    --bufsize 10MB --curcache 100MB --mem 16384MB --threads 96 \
-    --progress --verbose --details --log-level debug;
-        then
-            echo "Downloaded fastq files for $1"
-        else
-            echo "Download failed for $1"
-            exit 1
+    
+    # Download compressed sra files
+    if prefetch "$1" --output-directory data/ \
+    --verbose --progress \
+    --resume yes -L debug;
+    then
+        echo "Downloaded fastq files for $1"
+    else
+        echo "Download failed for $1"
+        exit 1
     fi
-
+    
+    # Dump fastq files from sra
+    if fasterq-dump "data/$1/$1.sra" --outdir pairs/ --temp tmp/ \
+    --bufsize 10MB --curcache 100MB --mem 4000MB --threads 96 \
+    --progress --verbose --details --log-level debug;
+    then
+        echo "Extracted fastq files for $1"
+    else
+        echo "Extraction failed for $1"
+        exit 1
+    fi
+    
     # Run bowtie2 on downloaded files
     if [ -f "pairs/$1_1.fastq" ] && [ -f "pairs/$1_2.fastq" ]; then
         if bowtie2 --threads 96 --time -x db/xylo \
-            -q --phred33 --local --very-sensitive-local --no-unal \
-            -N 1 -L 12 --rfg 5,2 \
-            -1 "pairs/$1_1.fastq" -2 "pairs/$1_2.fastq" -S aln/"$1".sam;
-            then
-                echo "Bowtie2 align completed for $1"
-                echo "Deleting fastq files"
-                rm -f pairs/"$1"_{1,2}.fastq
-            else
-                echo "Bowtie2 align failed for $1"
-                exit 1
-            fi
+        -q --phred33 --local --very-sensitive-local --no-unal \
+        -N 1 -L 12 --rfg 5,2 \
+        -1 "pairs/$1_1.fastq" -2 "pairs/$1_2.fastq" -S aln/"$1".sam;
+        then
+            echo "Bowtie2 align completed for $1"
+            echo "Deleting sra files"
+            rm -rf data/"$1"
+            echo "Deleting fastq files"
+            rm -f pairs/"$1"_{1,2}.fastq
+        else
+            echo "Bowtie2 align failed for $1"
+            exit 1
+        fi
     else
         echo "Incomplete pairs present for $1"
         exit 1
     fi
-
+    
     echo "Finished alignment for $1"
-
+    
 }
 
 export -f align
 
 srun parallel --joblog logs/parallel --tmpdir tmp/ \
-    --compress --keep-order --group \
-    --retries 3 --jobs 1 --arg-file runs align
+--compress --keep-order --group \
+--retries 3 --jobs 1 --arg-file runs align

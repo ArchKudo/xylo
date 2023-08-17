a83e0047039666a829c29d66e8855af5c301f73c
Girish Vλas [gdv1] <gdv1@aber.ac.uk>
2023-08-17 17:42:17 +0100

    Test why srun won't run align

---
+++ scripts/test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=bowtie
+#SBATCH --output=logs/gdv.bowtie.out.%J
+#SBATCH --error=logs/gdv.bowtie.err.%J
+
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=gdv1@aber.ac.uk
+
+#SBATCH --partition=cpubig
+#SBATCH --ntasks=96
+
+# Setup checks
+# echo "No srun $(/bin/hostname)"
+# srun echo "With srun $(/bin/hostname)"
+
+function align {
+	echo "Step 1: $1"
+	echo "Step 2: $1"
+	echo "Step 3: $3"
+}
+
+export -f align
+
+run=$(sed -n "${SLURM_ARRAY_TASK_ID}"p runs)
+
+align $run

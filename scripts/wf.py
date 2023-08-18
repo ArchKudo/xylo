b0567aef9a4df8015d9c7cbff74020a7e59a23f4
Girish Vλas [gdv1] <gdv1@aber.ac.uk>
2023-08-18 23:53:36 +0100

    Drop py files

--- scripts/wf.py
+++
@@ -1,8 +0,0 @@
-from Bio import Entrez
-from Bio import SeqIO
-
-Entrez.email = "your_email@example.com"
-
-search = Entrez.esearch(db="nucleotide", term="xylose reductase (xyl1) gene, complete cds", retmax=200)
-records = Entrez.read(search)
-ids = records["IdList"]
